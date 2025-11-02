85b56e445459636b1809f07bade9ff9b12aa5734
Arne Link <mail@arne-link.de>
2025-11-02 10:51:48 +0100

    use sudo for apk add

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install ruby, required for homebrew
-apk add ruby
+sudo apk add ruby
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
